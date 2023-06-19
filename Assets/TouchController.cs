19d4fef10b7088cc2498a4d9c20a99d585ebfff3
Yutaka Fukui <fukui@iittff.com>
2023-06-19 15:18:28 +0900

    2023/06/19

--- Assets/TouchController.cs
+++ Assets/TouchController.cs
@@ -4,48 +4,79 @@ using System.Net.Sockets;               // add
 using System.Text;
 using UnityEngine;
 
-
 public class TouchController : MonoBehaviour
 {
-    public string serverAddress = "192.168.0.101";  // EV3
+    public string serverAddress = "192.168.0.101";  // EV3のIPアドレス
     public int port = 8001;
     public SpriteRenderer touchPoint = null;
 
     private TcpClient tcpClient;
     private NetworkStream networkStream;
 
-    // Start is called before the first frame update
+    public Vector2 startPos;
+
     void Start()
     {
-        tcpClient = new TcpClient(serverAddress, port);
-        networkStream = tcpClient?.GetStream();
-        Debug.Log("Connected");
+        //tcpClient = new TcpClient(serverAddress, port);
+        //networkStream = tcpClient?.GetStream();
+        //Debug.Log("Connected");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount == 1)
+        if (Input.touchCount == 1)  // シングルタップ1点
         {
             var touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
+
+            switch (touch.phase)
             {
-                var pos = touch.position;
-                Debug.Log(pos);
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    break;
+                case TouchPhase.Moved:
+                    var pos = touch.position;
+                    // スクリーン座標からワールド座標
+                    var screenPos = new Vector3(pos.x, pos.y, 5f);
+                    var worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+                    touchPoint.transform.position = worldPos;
 
-                // スクリーン座標からワールド座標
-                var screenPos = new Vector3(pos.x, pos.y, 5f);
-                var worldPos = Camera.main.ScreenToWorldPoint(screenPos);
-                touchPoint.transform.position = worldPos;
+                    // ロボットへ送信
+                    //var buffer = Encoding.UTF8.GetBytes("MOVE");
+                    //networkStream?.Write(buffer, 0, buffer.Length);
+                    //Debug.Log("MOVE");
 
-                // ロボットへ送信
-                var buffer = Encoding.UTF8.GetBytes("MOVE");
-                networkStream?.Write(buffer, 0, buffer.Length);
-                Debug.Log("MOVE");
-
+                    break;
+                case TouchPhase.Ended:
+                    break;
             }
-        } else if (Input.touchCount == 2)
+
+            //if (touch.phase == TouchPhase.Began)
+            //{
+            //    var pos = touch.position;
+            //    Debug.Log(pos);
+
+            //    // スクリーン座標からワールド座標
+            //    var screenPos = new Vector3(pos.x, pos.y, 5f);
+            //    var worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+            //    touchPoint.transform.position = worldPos;
+
+            //    // ロボットへ送信
+            //    var buffer = Encoding.UTF8.GetBytes("MOVE");
+            //    networkStream?.Write(buffer, 0, buffer.Length);
+            //    Debug.Log("MOVE");
+
+            //}
+        }
+        else if (Input.touchCount == 2) // マルチタップ2点
         {
+            // ソケット通信を開始する
+            tcpClient = new TcpClient(serverAddress, port);
+            networkStream = tcpClient?.GetStream();
+            Debug.Log("Connected");
+        }
+        else if (Input.touchCount == 3) // マルチタップ3点
+        {
+            // 終了処理
             var buffer = Encoding.UTF8.GetBytes("exit");
             networkStream?.Write(buffer, 0, buffer.Length);
             Debug.Log("exit");
